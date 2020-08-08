22f34cda487012431b01f5aa71430a230f74a4ae
Liliana <ayalilian2018@gmail.com>
2020-08-08 19:10:55 -0300

    finish exercise five

--- exercise-five/src/ar/com/ada/online/second/exercisefive/Main.java
+++ exercise-five/src/ar/com/ada/online/second/exercisefive/Main.java
@@ -1,5 +1,5 @@
 package ar.com.ada.online.second.exercisefive;
-//5. Se tiene el nombre y la edad de tres personas. Se desea saber el nombre y la edad de la
+ //Se tiene el nombre y la edad de tres personas. Se desea saber el nombre y la edad de la
 //persona de menor edad.
 
 import java.util.Scanner;
@@ -7,49 +7,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String nameone= " ";
-        String namedos = " ";
-        String nametres= " ";
+        String firstName;
+        String secondName;
+        String thirdName;
         int firstAge = 0;
         int secondAge = 0;
         int thirdAge = 0;
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Ingrese edad ");
+        System.out.println("Ingrese el primer nombre ");
+        firstName = keyboard.next();
+
+
+        System.out.println(" Ingrese primer edad ");
         firstAge = keyboard.nextInt();
 
-        System.out.println("Ingrese nombre uno");
-        nameone = keyboard.nextLine();
-        keyboard.nextLine();
+        System.out.println(" Ingrese el segundo nombre");
+        secondName = keyboard.next();
 
 
-        System.out.println("Ingrese edad 2 ");
+
+        System.out.println(" Ingrese segunda edad ");
         secondAge = keyboard.nextInt();
-        System.out.println("Ingrese nombre dos");
-        namedos = keyboard.nextLine();
-        keyboard.nextLine();
+
+        System.out.println(" Ingrese tercer nombre ");
+        thirdName = keyboard.next();
 
 
-
-        System.out.println("Ingrese edad 3 ");
+        System.out.println(" Ingrese tercera edad");
         thirdAge = keyboard.nextInt();
-        System.out.println("Ingrese nombre tres");
-        nametres = keyboard.nextLine();
-        keyboard.nextLine();
 
 
         if (firstAge < secondAge && firstAge < thirdAge) {
 
-            System.out.println("La edad menor es: " + firstAge);
-            System.out.println("el nombre es: " + nameone);
-        }else{
-            if (secondAge < firstAge && secondAge < thirdAge) {
-                System.out.println("La edad menor es: " + secondAge);
-                System.out.println("el nombre es: " + namedos);
-            }else{
-                System.out.println("La edad menor es: " + thirdAge );
-                System.out.println("el nombre es: " + nametres);
+            System.out.println(" La edad menor es: " + firstAge + " y el nombre es " + firstName);
+
+
+        } else  if (secondAge < firstAge && secondAge < thirdAge) {
+                System.out.println(" La edad menor es: " + secondAge + " y el nombre es " + secondName);
+
+
+            } else {
+                System.out.println(" La edad menor es: " + thirdAge + " y el nombre es " + thirdName);
+
 
             }
 
@@ -57,4 +58,4 @@ public class Main {
 
 
     }
-}
+
